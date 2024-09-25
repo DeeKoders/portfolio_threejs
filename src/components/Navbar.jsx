f74b7dd5cff845cc95fbaea558b961070cf808f1
danyal-deekoders <deekoders@gmail.com>
2024-09-25 15:15:38 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
   const GITHUB_URL = "https://github.com/DeeKoders";
+  const test = "";
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
